7add620ba83b98b4485ff58fb5838abc56e60375
bahsk <bahskzs610@live.com>
2022-06-07 22:18:01 +0800

    2022-06-07 es7 整合RestHighLevelClient

--- src/main/java/com/yqy/bootes/entity/Hotel.java
+++ src/main/java/com/yqy/bootes/entity/Hotel.java
@@ -1,8 +1,8 @@
 package com.yqy.bootes.entity;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.elasticsearch.annotations.Document;
 
 /**
  * @author bahsk
@@ -11,12 +11,15 @@ import org.springframework.data.elasticsearch.annotations.Document;
  * @program: boot-es
  */
 
-@Document(indexName = "hotel")
+//@Document(indexName = "hotel")
 @Data
 public class Hotel {
-    @Id                 //对应Elasticsearch的_id
+//    @Id                 //对应Elasticsearch的_id
     String id;
-    String title;      //对应索引中的title
-    String city;       //对应索引中的city
-    String price;      //对应索引中的price
+    String index;                                               //对应索引名称
+    Float score;                                                //对应文档得分
+
+    String title;                                               //对应索引中的title
+    String city;                                                //对应索引中的city
+    Double price;                                               //对应索引中的price
 }
--- src/main/java/com/yqy/bootes/service/EsService.java
+++ src/main/java/com/yqy/bootes/service/EsService.java
@@ -1,11 +1,22 @@
 package com.yqy.bootes.service;
 
 import com.yqy.bootes.entity.Hotel;
-import com.yqy.bootes.repo.EsRepository;
+//import com.yqy.bootes.repo.EsRepository;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author bahsk
@@ -17,11 +28,50 @@ import java.util.List;
 @Service
 public class EsService {
 
+//    @Resource
+//    private EsRepository esRepository;
+
+//    public List<Hotel> getHotelFromTitle(String keyword){
+//        return  esRepository.findByTitleLike(keyword);//调用搜索方法
+//
+//    }
+
+
     @Resource
-    private EsRepository esRepository;
+    private RestHighLevelClient restHighLevelClient;
 
     public List<Hotel> getHotelFromTitle(String keyword){
-        return  esRepository.findByTitleLike(keyword);//调用搜索方法
+        SearchRequest searchRequest=new SearchRequest("hotel");//客户端请求
+        SearchSourceBuilder searchSourceBuilder=new SearchSourceBuilder();
+        //构建query
+        searchSourceBuilder.query(QueryBuilders.matchQuery("title",keyword));
+        searchRequest.source(searchSourceBuilder);
+        List<Hotel> resultList=new ArrayList<Hotel>();
+        try{
+            SearchResponse searchResponse=restHighLevelClient.search(searchRequest,
+                    RequestOptions.DEFAULT);
+            RestStatus status = searchResponse.status();
+            if(status!=RestStatus.OK){
+                return null;
+            }
+            SearchHits searchHits=searchResponse.getHits();
+            for(SearchHit searchHit:searchHits){
+                Hotel hotel=new Hotel();
+                hotel.setId(searchHit.getId());                                 //文档_id
+                hotel.setIndex(searchHit.getIndex());                           //索引名称
+                hotel.setScore(searchHit.getScore());                           //文档得分
 
+                //转换为Map
+                Map<String, Object> dataMap= searchHit.getSourceAsMap();
+                hotel.setTitle((String) dataMap.get("title"));  //设置标题
+                hotel.setCity((String) dataMap.get("city"));    //设置城市
+                hotel.setPrice((Double) dataMap.get("price"));  //设置价格
+                resultList.add(hotel);
+            }
+            return resultList;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
